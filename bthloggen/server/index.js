791635ef6299d48a83da329bf356008eb94d3114
gmiak <kayss.g@gmail.com>
2022-10-16 19:30:57 +0200

    The route "Get /data" is implemented!

--- bthloggen/server/index.js
+++ bthloggen/server/index.js
@@ -8,11 +8,46 @@ app.get("/", (req, res) => {
     res.json({
         "/": "Shows a list of all posible routes that are used.",
         "/data": "Shows our data collection.",
-        "/data?ip=<ip>": "Shows data that contain the given Ip address <ip>.",
-        "/data?url=<url>": "Shows data that contain the given Http/Https address <url>."
+        "/data?ip=<ip>": "Shows data that contains the given Ip address <ip>.",
+        "/data?url=<url>": "Shows data that contains the given http/https address <url>."
     });
 });
 
+app.get("/data", (req, res) => {
+    const itemsfile = require("../data/log.json");
+    let result = [{"Message": "Data loading..."}]
+    if (req.query.ip) {
+        console.log("search ip: "+req.query.ip+" in Db.");
+        result = itemsfile.filter(item => item.ip.includes(req.query.ip));
+    } else if (req.query.url) {
+        console.log("search url: "+req.query.url+" in Db.");
+        result = itemsfile.filter(item => item.url.includes(req.query.url));
+    } else {
+        let tempObejctArr = []
+        for (let index = 0; index < 100; index++) {
+            tempObejctArr[index] = itemsfile[index];
+        }
+        console.log("Fetced the first 100th items.")
+        result = tempObejctArr;
+    }
+    res.json(result);
+});
+
+/*app.get("/data/:ip", (req, res) => {
+    const itemsfile = require("../data/log.json");
+
+    let result = itemsfile.filter(item => item.ip.includes(req.params.ip));
+
+    res.json(result);
+});
+
+app.get("/data/:url", (req, res) => {
+    const itemsfile = require("../data/log.json");
+
+    let result = itemsfile.filter(item => item.url.includes(req.params.url));
+
+    res.json(result);
+});*/
 
 app.listen(port);
 console.log('The server is now listening on: ' + port);
