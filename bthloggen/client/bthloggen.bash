36f9fee5e6a91819c4e2a1c8ab1630b396c452e3
gmiak <kayss.g@gmail.com>
2022-10-18 20:33:29 +0200

    Requirement3: done!

---
+++ bthloggen/client/bthloggen.bash
@@ -0,0 +1,204 @@
+#!/usr/bin/env bash 
+
+#
+# A template for creating command line scripts taking options, commands
+# and arguments.
+#
+# Exit values:
+#  0 on success
+#  1 on failure
+#
+
+#
+# A globale variable for the name of the script
+#
+SCRIPT=$( basename "$0" )
+
+#
+# Current version of the script
+#
+VERSION="1.0.0"
+
+#
+# Set environment and use defaults if defined
+#
+DBWEBB_HOST=$(head -n 1 server.txt)
+DBWEBB_PORT=${DBWEBB_PORT:-1338}
+
+#
+# Message to display for usage and help 
+#
+function usage 
+{
+    local txt=( 
+        ""
+        " Utility $SCRIPT for sending request to the server."
+        " Usage: $SCRIPT [options] <command> [arguments]."
+        ""
+        " Commands available:"
+        "    url                        Get url to view the server in browser."
+        "    view                       List all entries."
+        "    view url <url>             View all entries containing <url>."
+        "    view ip <ip>               View all entries containing <ip>."
+        "    use <server>               Sets the servername (localhost or service name)."  
+
+        ""
+        " Options:"
+        "    --help, -h          Display the menu."
+        "    --version, -v       Display the current version."
+        "    --count, -c         Display the number of rows returned."
+        ""
+    )
+    printf "%s\n" "${txt[@]}"
+}
+
+#
+# Message to display when bad usage
+# Gets an argument or mot, e,g badUsage "Options/command not recognized." or badUsage
+#
+function badUsage
+{
+    local message=" $1"
+    local txt=(
+        ""
+        " For an overview of the command, execute:"
+        " ./$SCRIPT --help, -h"
+        ""
+    )
+
+    [[ $message ]] && echo "$message"
+    printf '%s\n' "${txt[@]}"
+}
+
+#
+# Message to display for version
+#
+function version
+{
+    local txt=(
+        ""
+        " $SCRIPT version $VERSION"
+        ""
+    )
+    printf "%s\n" "${txt[@]}"
+}
+
+#
+# count func
+#
+function count
+{
+    if [[ "$2" = "view" ]]
+    then
+        if [[ "$3" = "url" ]]
+        then
+            echo ""
+            curl -s "http://$DBWEBB_HOST:$DBWEBB_PORT/data?url=$4"  | jq '. | length' && echo
+            exit 0
+        fi
+        if [[ "$3" = "ip" ]]
+        then
+            echo ""
+            curl -s "http://$DBWEBB_HOST:$DBWEBB_PORT/data?ip=$4"  | jq '. | length' && echo
+            exit 0
+        fi
+        if [[ -z "$4" ]]
+        then
+            echo ""
+            curl -s "http://$DBWEBB_HOST:$DBWEBB_PORT/data"  | jq '. | length' && echo
+            exit 0
+        fi
+        badUsage "Options/command not reconized."
+        exit 1  
+    fi
+    badUsage "Options/command not reconized."
+    exit 1  
+}
+
+#
+# url func
+#
+function app-url
+{
+    echo ""
+    echo "http://$DBWEBB_HOST:$DBWEBB_PORT"
+    echo ""
+    exit 0
+}
+
+#
+# view func
+#
+function app-view
+{
+    if [[ "$1" = "url" ]]
+    then
+        curl "http://$DBWEBB_HOST:$DBWEBB_PORT/data?url=$2" | jq '.' 
+        exit 0
+    elif [[ "$1" = "ip" ]]
+    then
+        curl "http://$DBWEBB_HOST:$DBWEBB_PORT/data?ip=$2" | jq '.' 
+        exit 0
+    else 
+        curl "http://$DBWEBB_HOST:$DBWEBB_PORT/data" | jq '.' 
+        exit 0
+    fi
+}
+
+#
+# use func
+#
+function app-use
+{
+    
+    if [[ $1 ]]
+    then
+        echo "$1" >server.txt
+        echo "Server is now: $1"
+        exit 0
+    fi  
+    badUsage "Options/command not reconized."
+    exit 1  
+}
+
+
+#
+# Checks arguments that are send into the script
+# Uses while-loop and case-state to catch different commands and arguments.
+#
+while (( $#))
+do 
+    case "$1" in
+        --help | -h)
+            usage
+            exit 0
+        ;;
+
+        --version | -v)
+            version
+            exit 0
+        ;;
+
+        --count | -c)
+            count "$@"
+            exit 0
+        ;;
+    # more code
+        url       \
+        | view   \
+        | use   )           
+            command=$1       
+            shift               #  We present our command as one collection         
+            app-"$command" "$@"    #  if we get a match, we call a function app-$command $*
+            exit 0              #  Where $command = command_name (e.g, app-commit) and $* is the list of arguments
+        ;;
+
+        *)
+            badUsage "Options/command not reconized."
+            exit 1
+        ;;
+    esac
+done
+
+badUsage
+exit 1
