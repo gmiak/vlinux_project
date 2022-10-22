8e2095469d35ee5431724ac7314f247282db639c
gmiak <kayss.g@gmail.com>
2022-10-22 02:03:26 +0200

    Docker images: server, client and web client builded!

--- bthloggen/log2json.bash
+++ bthloggen/log2json.bash
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
 #
-# Run the first requirement
+# Initializing
 #
 awk -f req1.awk ../access-50k.log # Loads Url and Ip from file
 python3 req1_fileTextToJsonFile.py # Loads Url and Ip from Text-file into Json-file (In Json-format)
 cp log.json data/
+
+#
+# Starting server, client and web client service
+#
+sudo docker-compose up -d server
+sudo docker-compose up -d webbclient
+sudo docker-compose run client
+
+#
+# Close server, client and web client service
+#
+sudo docker-compose down
